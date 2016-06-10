2b050a8a090f50625d3bfef0a1d5f7c987558717
Alex Szczuczko <aszczucz@redhat.com>
2016-06-10 13:52:07 -0600

    Improve help information in SelfExecutingOptions

--- distro/selfexecuting/src/main/java/org/wildfly/extras/patch/internal/SelfExecutingOptions.java
+++ distro/selfexecuting/src/main/java/org/wildfly/extras/patch/internal/SelfExecutingOptions.java
@@ -8,7 +8,7 @@ final class SelfExecutingOptions {
     @Option(name = "--help", help = true)
     boolean help;
 
-    @Option(name = "--server", usage = "Path to the target server directory. Defaults to $JBOSS_HOME or $CWD.")
+    @Option(name = "--server", metaVar = "DIR", usage = "Path to the target server directory. Defaults to $JBOSS_HOME or $CWD.")
     File serverHome;
 
     @Option(name = "--query-repository", usage = "Query the repository for available patches")
@@ -17,19 +17,22 @@ final class SelfExecutingOptions {
     @Option(name = "--query-server", usage = "Query the server for installed patches")
     boolean queryServer;
 
-    @Option(name = "--query-server-paths", usage = "Query managed server paths")
+    @Option(name = "--query-server-paths", metaVar = "PREFIX", usage = "Query managed server paths")
     String queryServerPaths;
 
     @Option(name = "--audit-log", usage = "Print the audit log")
     boolean auditLog;
 
-    @Option(name = "--install", forbids = { "--update", "--uninstall" },  usage = "Install the given patch id to the server")
+    @Option(name = "--install", metaVar = "PKG", forbids = { "--update",
+            "--uninstall" }, usage = "Install the given patch id to the server. Example of PKG: somepkg-1.0.0")
     String installId;
 
-    @Option(name = "--update", forbids = { "--install", "--uninstall" },  usage = "Update the server for the given patch name")
+    @Option(name = "--update", metaVar = "PKG", forbids = { "--install",
+            "--uninstall" }, usage = "Update the server for the given patch name. Examples of PKG: somepkg")
     String updateName;
 
-    @Option(name = "--uninstall", forbids = { "--install", "--update" },  usage = "Uninstall the given patch id from the server")
+    @Option(name = "--uninstall", metaVar = "PKG", forbids = { "--install",
+            "--update" }, usage = "Uninstall the given patch id from the server. Example of PKG: somepkg-1.0.0")
     String uninstallId;
 
     @Option(name = "--force", usage = "Force an --install or --update operation")
